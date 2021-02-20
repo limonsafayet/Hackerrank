5e72d2bf42959c0dcd9714533188db3367abe5f0
limonsafayet <limon6531@gmail.com>
2021-02-20 16:18:16 +0600

    Designer Mat

---
+++ Python/Strings/Designer_Door_Mat.py
@@ -0,0 +1,62 @@
+# Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+# Mat size must be N X M. (N is an odd natural number, and M is 3 times N.)
+# The design should have 'WELCOME' written in the center.
+# The design pattern should only use |, . and - characters.
+# Sample Designs
+
+#     Size: 7 x 21 
+#     ---------.|.---------
+#     ------.|..|..|.------
+#     ---.|..|..|..|..|.---
+#     -------WELCOME-------
+#     ---.|..|..|..|..|.---
+#    ------.|..|..|.------
+#     ---------.|.---------
+    
+#     Size: 11 x 33
+#     ---------------.|.---------------
+#    ------------.|..|..|.------------
+#     ---------.|..|..|..|..|.---------
+#     ------.|..|..|..|..|..|..|.------
+#     ---.|..|..|..|..|..|..|..|..|.---
+#     -------------WELCOME-------------
+#     ---.|..|..|..|..|..|..|..|..|.---
+#     ------.|..|..|..|..|..|..|.------
+#     ---------.|..|..|..|..|.---------
+#     ------------.|..|..|.------------
+#     ---------------.|.---------------
+# Input Format
+
+# A single line containing the space separated values of N and M.
+
+# Constraints
+
+#  5 < N < 101
+# 15 < M < 303
+
+# Output Format
+
+# Output the design pattern.
+
+# Sample Input
+
+# 9 27
+# Sample Output
+
+# ------------.|.------------
+# ---------.|..|..|.---------
+# ------.|..|..|..|..|.------
+# ---.|..|..|..|..|..|..|.---
+# ----------WELCOME----------
+# ---.|..|..|..|..|..|..|.---
+# ------.|..|..|..|..|.------
+# ---------.|..|..|.---------
+# ------------.|.------------
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+N,M=map(int,input().split())
+a=[('.|.'*i).center(M,'-') for i in range(1,N,2)]
+for e in a+['WELCOME'.center(M,'-')]+list(reversed(a)):print(e)
+        
